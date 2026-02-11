98bf532aacd570eb25bc0e3478467c966f38f540
bhumii2908-coder <bhumii2908@gmail.com>
2026-02-11 14:38:53 +0530

    notes.cpp

---
+++ .vscode/notes11.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+int og=n;
+int temp=n;
+int sum=0;
+// digit calcution 
+int 1=0;
+while(temp!=0){
+    temp=temp/10;
+    i++;
+}
+// power niklana and add karna
+while(n!=0){
+    int k=n%10;
+    sum=sum+pow(k,l)
+    n=n/10;
+}
+// comparre krna
+if(og==sum){
+    cout<<"Armostrong"<<endl;
+}
+else{
+    cout<<"Not Armostrong"<<endl;
+}
+return 0;
+}
