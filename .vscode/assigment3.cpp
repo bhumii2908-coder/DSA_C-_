94e0feaad8b862bf4b50e72b5eda26191ee73d2b
bhumii2908-coder <bhumii2908@gmail.com>
2026-02-14 21:40:23 +0530

    printseries.cpp

---
+++ .vscode/assigment3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	
+	int n1,n2;
+	cin>>n1>>n2;
+	for(int i=2;i<=n1;i++){
+		int k=3*i+2;
+		if(k%n2==0){
+			n1++;
+		}
+		else{
+			cout<<k<<endl;
+		}
+	}
+	return 0;
+}
