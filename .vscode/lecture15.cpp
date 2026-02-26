34af0f4eb0e1af58d4a8e64ca7809338286522a4
bhumii2908-coder <bhumii2908@gmail.com>
2026-02-26 23:17:28 +0530

    wave_print.cpp

--- .vscode/lecture15.cpp
+++ .vscode/lecture15.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int r,c;
     cin>>r>>c;
-    vector<vector<int>> a(r,vector<int>(c));
+    vector<vector<int> >a(r,vector<int>(c));
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             cin>>a[i][j];
