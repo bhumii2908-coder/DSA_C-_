4f2a20b5d08f52451ac039de18ae0dd06f08e731
bhumii2908-coder <bhumii2908@gmail.com>
2026-03-06 23:46:50 +0530

    ReverseanArray.cpp

---
+++ hackerblock1.cpp
@@ -0,0 +1,21 @@
+// Arrays-Reverse an Array
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    reverse(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
