d49f6592716b5c2e6c1010382660fc9ef70131a9
bhumii2908-coder <bhumii2908@gmail.com>
2026-03-07 22:43:41 +0530

    Inversearray.cpp

---
+++ Hblock3.cpp
@@ -0,0 +1,21 @@
+// Inverse of an array
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> ans(n);
+    for(int i=0;i<n;i++){
+        ans[arr[i]]=i;
+    }
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+   return 0;
+}
