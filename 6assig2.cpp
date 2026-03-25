38d5c280eb6d8ca1dcac8a97384a3f6efdccd9e5
bhumii2908-coder <bhumii2908@gmail.com>
2026-03-25 13:35:48 +0530

    String /Remove_duplicate.cpp

---
+++ 6assig2.cpp
@@ -0,0 +1,16 @@
+// String
+// /Remove_duplicate.cpp
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+        for(int i=0;i<s.size();i++){
+        if(s[i]!=s[i+1]){
+            cout<<s[i];
+        }
+        }
+   return 0;
+}
