cf3564422a34bad8e5a6807879b952b8fb3146f8
bhumii2908-coder <bhumii2908@gmail.com>
2026-04-15 00:38:42 +0530

    191leetcode.cpp

---
+++ lecture43.cpp
@@ -0,0 +1,20 @@
+// 191leetcode.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int hammingWeight(int n) {
+    int count=0;
+    while(n>0){
+        if(n&1==1){
+            count++;
+        }
+        n=n>>1;
+    }
+    return count;
+}
+int main() {
+    int n=11;
+    cout<<hammingWeight(n)<<endl;
+    return 0;
+}
