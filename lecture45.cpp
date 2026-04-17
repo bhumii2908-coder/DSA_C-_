1eefafdfc73413f498c790554a400fc806d4c296
bhumii2908-coder <bhumii2908@gmail.com>
2026-04-17 22:29:50 +0530

    231leetcode.cpp

---
+++ lecture45.cpp
@@ -0,0 +1,12 @@
+// 231leetcode.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool isPowerOfTwo(int n) {
+    return n>0 and (n&(n-1))==0;
+}
+int main() {
+    cout<<isPowerOfTwo(16)<<endl;
+    return 0;
+}
